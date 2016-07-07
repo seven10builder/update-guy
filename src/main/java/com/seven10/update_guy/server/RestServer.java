45d44c56ac1adeea76dab1057e505ba74c0d7488
seven10builder <leeroyjenkins@seven10storage.com>
2016-07-07 18:13:02 -0400

    added multiple endpoints to server, took out some unneeded dependencies

--- src/main/java/com/seven10/update_guy/server/RestServer.java
+++ src/main/java/com/seven10/update_guy/server/RestServer.java
@@ -1,13 +1,29 @@
 package com.seven10.update_guy.server;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import com.seven10.update_guy.server.manifest.ManifestServlet;
+import com.seven10.update_guy.server.release.ReleaseServlet;
 import com.seven10.update_guy.server.repository.RepositoryServlet;
 
 public class RestServer
 {
+	private static String createServletsString()
+	{
+		List<String> servlets = new ArrayList<String>();
+		servlets.add(RepositoryServlet.class.getCanonicalName());
+		servlets.add(ManifestServlet.class.getCanonicalName());
+		servlets.add(ReleaseServlet.class.getCanonicalName());
+		
+		String s = String.join("; ", servlets);
+		return s;
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
@@ -19,7 +35,8 @@ public class RestServer
 		ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
 		jerseyServlet.setInitOrder(0);
 
-		jerseyServlet.setInitParameter("jersey.config.server.provider.classnames", RepositoryServlet.class.getCanonicalName());
+		String servlets = createServletsString();
+		jerseyServlet.setInitParameter("jersey.config.server.provider.classnames", servlets);
 
 		try
 		{
@@ -31,4 +48,6 @@ public class RestServer
 			jettyServer.destroy();
 		}
 	}
+
+
 }
